b91dcf0b51f721135b38509cedd2d3d90e93cae0
Jeatheak <thomas.heuvel+github@gmail.com>
2022-07-03 12:28:12 +0000

    feat: remove java files

--- test.java
+++
@@ -1,9 +0,0 @@
-public class test {
-    public static void main(String[] args) {
-        AirconStat stat = new AirconStat();
-
-        String data = AirconStatCoder.toBase64(stat);
-
-        System.out.println(data);
-    }
-}
