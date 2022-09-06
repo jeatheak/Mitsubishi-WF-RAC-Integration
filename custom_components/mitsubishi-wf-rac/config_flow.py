b43f081cc0af2b68f119c84b772c4fca6b789a12
Rob <github@robisawesome.com>
2022-09-06 21:27:59 +0100

    Config flow bugfix (#19)
     * When adding devices, the host (IP) was being matched against
       existing entries by substring match rather than exact match, so eg
       trying to add 1.2.3.4 would fail if there was already an entry with
       IP 1.2.3.40.
    
     * When adding discovered devices, some errors were being set with a
       key that doesn't exist, eg the "host already configured" error was
       being set to CONF_HOST, but that field isn't in the form that gets
       displayed to the user, so there was no error message displayed.
    
     * When manually adding a device, set the default values to whatever
       the user input was (eg if the user gives a bad name, or typos the
       IP, show the error but don't reset the form to defaults).

--- custom_components/mitsubishi-wf-rac/config_flow.py
+++ custom_components/mitsubishi-wf-rac/config_flow.py
@@ -4,6 +4,7 @@ from __future__ import annotations
 import logging
 from typing import Any
 from uuid import uuid4
+from functools import partial
 
 import voluptuous as vol
 
@@ -26,18 +27,6 @@ from .wfrac.repository import Repository
 
 _LOGGER = logging.getLogger(__name__)
 
-DATA_SCHEMA = vol.Schema(
-    {
-        vol.Required(
-            CONF_NAME, description={"suggested_value": "Airco unknown"}
-        ): cv.string,
-        vol.Required(CONF_HOST): cv.string,
-        vol.Optional(CONF_PORT, default=51443): cv.port,
-        vol.Optional(CONF_FORCE_UPDATE, default=False): cv.boolean,
-    }
-)
-
-
 class WfRacConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
     """Handle a config flow."""
 
@@ -46,34 +35,28 @@ class WfRacConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
     _discovery_info = {}
     DOMAIN = DOMAIN
 
-    async def _async_validate_input(
+    def _find_entry_matching(self, key, matches):
+        """Returns the first entry where matches(entry.data[key]) returns True"""
+        for entry in self._async_current_entries():
+            if key in entry.data and matches(entry.data[key]):
+                return entry
+        return None
+
+    async def _async_register_airco(
         self, hass: HomeAssistant, data: dict
     ) -> dict[str, Any]:
-        """Validate the user input allows us to connect."""
+        """Validate the user input allows us to connect, and register with the airco device"""
         if len(data[CONF_HOST]) < 3:
             raise InvalidHost
 
         if len(data[CONF_NAME]) < 3:
             raise InvalidName
 
-        for entry in self._async_current_entries():
-            already_configured = False
-            _device = None
-
-            if (
-                not data.get(CONF_FORCE_UPDATE)
-                and CONF_HOST in entry.data
-                and entry.data[CONF_HOST] in (data[CONF_HOST])
-            ):
-                # Is this address or IP address already configured?
-                already_configured = True
-                _device = entry.data
-
-            if already_configured:
-                raise HostAlreadyConfigured(
-                    f"Already configured {CONF_HOST}",
-                    device=_device,
-                )
+        if not data.get(CONF_FORCE_UPDATE):
+            # Is this hostname or IP address already configured?
+            existing_entry = self._find_entry_matching(CONF_HOST, lambda h: h == data[CONF_HOST])
+            if existing_entry:
+                raise HostAlreadyConfigured(error_name=existing_entry.data[CONF_NAME])
 
         repository = Repository(
             data[CONF_HOST],
@@ -82,10 +65,14 @@ class WfRacConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
             data[CONF_DEVICE_ID],
         )
 
-        airco_id = await hass.async_add_executor_job(repository.get_details)
+        try:
+            airco_id = await hass.async_add_executor_job(repository.get_details)
+        except Exception as query_failed:
+            raise CannotConnect(reason=str(query_failed)) from query_failed
+
         data[CONF_AIRCO_ID] = airco_id
         if not airco_id:
-            raise CannotConnect
+            raise CannotConnect(reason="unknown reason")
 
         _LOGGER.info(
             "Trying to register OperatorId[%s] on Airco[%s]",
@@ -98,71 +85,110 @@ class WfRacConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
         if not result:
             raise CannotConnect
         if int(result["result"]) == 2:
-            raise ToManyDevicesRegistered
+            raise TooManyDevicesRegistered
 
-        return {"title": data[CONF_NAME]}
+        return data
 
     async def _async_fetch_operator_id(self):
         """Fetch UUID operator id if exists otherwise create it"""
-        for entry in self._async_current_entries():
-            if CONF_OPERATOR_ID in entry.data:
-                return entry.data[CONF_OPERATOR_ID]
-
+        entry = self._find_entry_matching(CONF_OPERATOR_ID, bool)
+        if entry:
+            return entry.data[CONF_OPERATOR_ID]
         return f"hassio-{str(uuid4())[7:]}"
 
     async def _async_fetch_device_id(self):
         """Fetch unique device id if exists otherwise create it"""
-        for entry in self._async_current_entries():
-            if CONF_DEVICE_ID in entry.data:
-                return entry.data[CONF_DEVICE_ID]
-
+        entry = self._find_entry_matching(CONF_DEVICE_ID, bool)
+        if entry:
+            return entry.data[CONF_DEVICE_ID]
         return f"homeassistant-device-{uuid4().hex[21:]}"
 
-    async def async_step_discovery_confirm(self, user_input=None):
-        """Handle the initial step."""
-
+    async def _async_create_common(
+            self,
+            step_id: str,
+            data_schema: vol.Schema = None,
+            user_input: dict[str, Any] | None = None,
+            description_placeholders : dict[str, str] | None = None
+    ):
+        """Create a new entry"""
         errors = {}
-        if user_input is not None:
-            try:
-                user_input[CONF_HOST] = self._discovery_info[CONF_HOST]
-                user_input[CONF_PORT] = self._discovery_info[CONF_PORT]
+        description_placeholders = description_placeholders or {}
 
+        if user_input:
+            description_placeholders["error_name"] = ""
+            try:
                 user_input[CONF_OPERATOR_ID] = await self._async_fetch_operator_id()
                 user_input[CONF_DEVICE_ID] = await self._async_fetch_device_id()
 
-                info = await self._async_validate_input(self.hass, user_input)
-
-                return self.async_create_entry(title=info["title"], data=user_input)
-            except CannotConnect:
-                errors[CONF_BASE] = "cannot_connect"
-            except InvalidHost:
-                errors[CONF_HOST] = "cannot_connect"
-            except HostAlreadyConfigured:
-                errors[CONF_HOST] = "host_already_configured"
-            except ToManyDevicesRegistered:
-                errors[CONF_BASE] = "to_many_devices_registered"
-            except InvalidName:
-                errors[CONF_NAME] = "name_invalid"
+                info = await self._async_register_airco(self.hass, user_input)
+                return self.async_create_entry(title=info[CONF_NAME], data=user_input)
+            except KnownError as error:
+                _LOGGER.exception("create failed")
+                errors, placeholders = error.get_errors_and_placeholders(data_schema.schema)
+                errors.update(errors)
+                description_placeholders.update(placeholders)
             except Exception:  # pylint: disable=broad-except
                 _LOGGER.exception("Unexpected exception")
-                errors[CONF_BASE] = "unknown"
+                errors[CONF_BASE] = "unexpected_error"
 
-        # If there is no user input or there were errors, show the form again, including any errors that were found with the input.
+        # If there is no user input or there were errors, show the form again, including any errors
+        # that were found with the input.
         return self.async_show_form(
-            step_id="discovery_confirm",
-            data_schema=vol.Schema(
-                {
-                    vol.Required(
-                        CONF_NAME, default=f"Airco {self._discovery_info[CONF_NAME]}"
-                    ): str,
-                }
-            ),
+            step_id=step_id,
+            data_schema=data_schema,
             errors=errors,
-            description_placeholders={
-                "id": self._discovery_info[CONF_NAME],
-                "host": self._discovery_info[CONF_HOST],
-                "port": self._discovery_info[CONF_PORT],
-            },
+            description_placeholders=description_placeholders
+        )
+
+    @staticmethod
+    def _field(user_input, name, which, default=None):
+        """Helper for creating schema fields"""
+        value = user_input.get(name, default) if user_input else default
+        description = None
+        if value is not None:
+            description = {"suggested_value": value}
+        return which(name, description=description)
+
+    async def async_step_discovery_confirm(self, user_input=None):
+        """Handle adding device discovered by zeroconf."""
+
+        description_placeholders = {
+            "id": self._discovery_info[CONF_NAME],
+            "host": self._discovery_info[CONF_HOST],
+            "port": self._discovery_info[CONF_PORT],
+        }
+
+        if user_input:
+            for key in [CONF_HOST, CONF_PORT]:
+                user_input[key] = self._discovery_info[key]
+
+        field = partial(self._field, user_input)
+        data_schema = vol.Schema({
+            field(CONF_NAME, vol.Required, f"Airco {self._discovery_info[CONF_NAME]}") : str,
+        })
+
+        return await self._async_create_common(
+            step_id="discovery_confirm",
+            data_schema=data_schema,
+            user_input=user_input,
+            description_placeholders=description_placeholders
+        )
+
+    async def async_step_user(self, user_input=None):
+        """Handle adding device manually."""
+
+        field = partial(self._field, user_input)
+        data_schema = vol.Schema({
+            field(CONF_NAME, vol.Required, "Airco unknown") : cv.string,
+            field(CONF_HOST, vol.Required) : cv.string,
+            field(CONF_PORT, vol.Optional, 51443): cv.port,
+            field(CONF_FORCE_UPDATE, vol.Optional, False): cv.boolean,
+        })
+
+        return await self._async_create_common(
+            step_id="user",
+            data_schema=data_schema,
+            user_input=user_input
         )
 
     async def async_step_zeroconf(
@@ -170,103 +196,83 @@ class WfRacConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
     ) -> FlowResult:
         """Handle zeroconf discovery."""
 
-        local_name = discovery_info.hostname[:-1]
+        local_name = discovery_info.hostname.rstrip(".")
         node_name = local_name[: -len(".local")]
         host = discovery_info.host
         port = discovery_info.port
 
+        _LOGGER.debug("zeroconf discovery: hostname=%r, host=%r, port=%r",
+                      discovery_info.hostname,
+                      discovery_info.host,
+                      discovery_info.port)
+
+        info = {CONF_HOST: host, CONF_PORT: port}
+
         await self.async_set_unique_id(node_name)
-        self._abort_if_unique_id_configured(updates={CONF_HOST: discovery_info.host})
+        self._abort_if_unique_id_configured(updates=info)
 
-        for entry in self._async_current_entries():
-            already_configured = False
+        existing_entry = self._find_entry_matching(CONF_HOST, lambda h: h == host)
+        if existing_entry:
+            _LOGGER.debug("already configured!")
+            return self.async_abort(reason="already_configured")
 
-            if CONF_HOST in entry.data and entry.data[CONF_HOST] in (
-                host,
-                discovery_info.host,
-            ):
-                # Is this address or IP address already configured?
-                already_configured = True
-
-            if already_configured:
-                return self.async_abort(reason="already_configured")
-
-        self._discovery_info = {CONF_HOST: host, CONF_NAME: node_name, CONF_PORT: port}
+        info[CONF_NAME] = node_name
+        self._discovery_info = info
 
         return await self.async_step_discovery_confirm()
-
-    async def async_step_user(self, user_input=None):
-        """Handle the initial step."""
-
-        errors = {}
-        arguments = None
-        if user_input is not None:
-            try:
-
-                user_input[CONF_OPERATOR_ID] = await self._async_fetch_operator_id()
-                user_input[CONF_DEVICE_ID] = await self._async_fetch_device_id()
-
-                info = await self._async_validate_input(self.hass, user_input)
-
-                _LOGGER.warning(
-                    "Got %s and %s",
-                    user_input[CONF_OPERATOR_ID],
-                    user_input[CONF_DEVICE_ID],
-                )
-
-                return self.async_create_entry(title=info["title"], data=user_input)
-            except CannotConnect:
-                errors[CONF_BASE] = "cannot_connect"
-            except InvalidHost:
-                errors[CONF_HOST] = "cannot_connect"
-            except HostAlreadyConfigured as argument:
-                arguments = argument
-                errors[CONF_HOST] = "host_already_configured"
-            except ToManyDevicesRegistered:
-                errors[CONF_BASE] = "to_many_devices_registered"
-            except InvalidName:
-                errors[CONF_NAME] = "name_invalid"
-            except Exception:  # pylint: disable=broad-except
-                _LOGGER.exception("Unexpected exception")
-                errors[CONF_BASE] = "unknown"
-
-        # If there is no user input or there were errors, show the form again, including any errors that were found with the input.
-        return self.async_show_form(
-            step_id="user",
-            data_schema=DATA_SCHEMA,
-            errors=errors,
-            description_placeholders={
-                "error_name": arguments.device[CONF_NAME]
-                if not arguments is None
-                else "",
-                # "ip": user_input[CONF_HOST],
-            },
-        )
 
     @property
     def _name(self) -> str | None:
         return self.context.get(CONF_NAME)
 
+# pylint: disable=too-few-public-methods
 
-class CannotConnect(exceptions.HomeAssistantError):
-    """Error to indicate we cannot connect."""
+class KnownError(exceptions.HomeAssistantError):
+    """Base class for errors known to this config flow.
 
+    [error_name] is the value passed to [errors] in async_show_form, which should match a key
+    under "errors" in strings.json
 
-class InvalidHost(exceptions.HomeAssistantError):
-    """Error to indicate there is an invalid hostname."""
+    [applies_to_field] is the name of the field name that contains the error (for
+    async_show_form); if the field doesn't exist in the form CONF_BASE will be used instead.
+    """
+    error_name = "unknown_error"
+    applies_to_field = CONF_BASE
 
-
-class HostAlreadyConfigured(exceptions.HomeAssistantError):
-    """Error to indicate there is an duplicate hostname."""
-
-    def __init__(self, *args: object, device) -> None:
+    def __init__(self, *args: object, **kwargs: dict[str, str]) -> None:
         super().__init__(*args)
-        self.device = device
+        self._extra_info = kwargs
 
+    def get_errors_and_placeholders(self, schema):
+        """Return dicts of errors and description_placeholders, for adding to async_show_form"""
+        key = self.applies_to_field
+        # Errors will only be displayed to the user if the key is actually in the form (or
+        # CONF_BASE for a general error), so we'll check the schema (seems weird there
+        # isn't a more efficient way to do this...)
+        if key not in {k.schema for k in schema}:
+            key = CONF_BASE
+        return ({key : self.error_name}, self._extra_info or {})
 
-class InvalidName(exceptions.HomeAssistantError):
+class CannotConnect(KnownError):
+    """Error to indicate we cannot connect."""
+    error_name = "cannot_connect"
+
+class InvalidHost(KnownError):
     """Error to indicate there is an invalid hostname."""
+    error_name = "cannot_connect"
+    applies_to_field = CONF_HOST
 
+class HostAlreadyConfigured(KnownError):
+    """Error to indicate there is an duplicate hostname."""
+    error_name = "host_already_configured"
+    applies_to_field = CONF_HOST
 
-class ToManyDevicesRegistered(exceptions.HomeAssistantError):
-    """Error to indicated that there are to many devices registered"""
+class InvalidName(KnownError):
+    """Error to indicate there is an invalid hostname."""
+    error_name = "name_invalid"
+    applies_to_field = CONF_NAME
+
+class TooManyDevicesRegistered(KnownError):
+    """Error to indicate that there are too many devices registered"""
+    error_name = "too_many_devices_registered"
+    applies_to_field = CONF_BASE
